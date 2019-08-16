742016483305d92a69efe2240ca724b10d8abdc3
Ace <15880168030@163.com>
2019-08-16 14:06:46 +0800

    首页页面设计

--- app/src/main/java/com/newland/karaoke/MainActivity.java
+++ app/src/main/java/com/newland/karaoke/MainActivity.java
@@ -2,14 +2,64 @@ package com.newland.karaoke;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //主界面四个按钮
+private ImageButton checkIn;
+private ImageButton order;
+private ImageButton settings;
+private ImageButton shift;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    void initUI(){
+        //初始化UI
+
+        checkIn=(ImageButton)findViewById(R.id.bt_checkin);
+        order=(ImageButton)findViewById(R.id.bt_order);
+        settings=(ImageButton)findViewById(R.id.bt_settings);
+        shift=(ImageButton)findViewById(R.id.bt_shift);
+
+        //设置监听
+        checkIn.setOnClickListener(this);
+        order.setOnClickListener(this);
+        settings.setOnClickListener(this);
+        shift.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //处理监听
+        switch(view.getId()){
+            case R.id.bt_checkin:
+                Intent intent_check=new Intent(MainActivity.this,CheckInActivity.class);
+                startActivity(intent_check);
+                break;
+            case R.id.bt_order:
+                Intent intent_order=new Intent(MainActivity.this,OrderActivity.class);
+                startActivity(intent_order);
+                break;
+            case R.id.bt_settings:
+                Intent intent_settings=new Intent(MainActivity.this,SettingActivity.class);
+                startActivity(intent_settings);
+                break;
+            case R.id.bt_shift:
+                Intent intent_shift=new Intent(MainActivity.this,ShiftActivity.class);
+                startActivity(intent_shift);
+                break;
+            default:
+                break;
+        }
     }
 }
