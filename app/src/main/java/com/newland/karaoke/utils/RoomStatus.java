7ca19217840a5247e61c39d7bca071e00fbb5ea9
Administrator <1281025009@qq.com>
2019-08-19 16:12:46 +0800

    变量类型修改,状态添加

---
+++ app/src/main/java/com/newland/karaoke/utils/RoomStatus.java
@@ -0,0 +1,6 @@
+package com.newland.karaoke.utils;
+
+public class RoomStatus {
+    public final static int FREE = 0;
+    public final static int NO_FREE = 1;
+}
---
+++ app/src/main/java/com/newland/karaoke/utils/RoomType.java
@@ -0,0 +1,12 @@
+package com.newland.karaoke.utils;
+
+/**
+ * 房间大小类型
+ */
+public class RoomType
+{
+    public final static int BIG = 0;
+    public final static int MIDDLE = 1;
+    public final static int SMAlL = 2;
+}
+
