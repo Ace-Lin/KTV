255c1b5d572a20a5766904f873016d0090471e8f
Ace <15880168030@163.com>
2019-08-20 17:25:25 +0800

    checkin界面设计

--- app/src/main/java/com/newland/karaoke/KTVApplication.java
+++ app/src/main/java/com/newland/karaoke/KTVApplication.java
@@ -3,7 +3,13 @@ package com.newland.karaoke;
 import android.app.Application;
 import android.content.Context;
 
+import com.newland.karaoke.constant.KTVType;
+import com.newland.karaoke.database.KTVRoomInfo;
+
 import org.litepal.LitePal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KTVApplication extends Application {
     private static Context context;
@@ -15,5 +21,24 @@ public class KTVApplication extends Application {
     }
     public static Context getContext(){
          return context;
+    }
+
+    //调试：初始化数据库
+    public void CreateDatabase(){
+        //List<KTVRoomInfo> ktvRoomInfo=new ArrayList<>();
+        KTVRoomInfo ktvRoomInfo1=new KTVRoomInfo();
+        ktvRoomInfo1.setRoom_name("A01");
+        ktvRoomInfo1.setRoom_price(100);
+        ktvRoomInfo1.setRoom_status(KTVType.RoomStatus.FREE);
+        ktvRoomInfo1.setRoom_type(KTVType.RoomType.MIDDLE);
+        ktvRoomInfo1.save();
+
+        KTVRoomInfo ktvRoomInfo2=new KTVRoomInfo();
+        ktvRoomInfo2.setRoom_name("A02");
+        ktvRoomInfo2.setRoom_price(200);
+        ktvRoomInfo2.setRoom_type(KTVType.RoomType.BIG);
+        ktvRoomInfo2.setRoom_status(KTVType.RoomStatus.FREE);
+        ktvRoomInfo2.save();
+
     }
 }
--- app/src/main/java/com/newland/karaoke/activity/CheckInActivity.java
+++ app/src/main/java/com/newland/karaoke/activity/CheckInActivity.java
@@ -2,7 +2,10 @@ package com.newland.karaoke.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.newland.karaoke.R;
 
@@ -12,5 +15,10 @@ public class CheckInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);//防止5.x以后半透明影响效果，使用这种透明方式
+        }
     }
+
 }
